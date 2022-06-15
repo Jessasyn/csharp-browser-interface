c3f7242e8e11c0fee50c420c62eba0e354480fd8
Jessasyn <alexf.prinsenakker@gmail.com>
2022-06-15 20:16:49 +0200

    Implements winapi call
    So that we can open a url on windows, too, without having to resort to opening a shell.
    This also removes the need for the filter function, so it has been removed.

--- BrowserInterface/BrowserHandler.cs
+++ BrowserInterface/BrowserHandler.cs
@@ -15,17 +15,18 @@ namespace BrowserInterface
     public sealed class BrowserHandler : IDisposable
     {
         /// <summary>
-        /// The <see cref="Array"/> of <see cref="char"/> which should be ignored for windows.
+        /// Performs an operation on a specified file. <br/>
+        /// Note: documentation from this function is taken directly from <seealso cref="https://docs.microsoft.com/nl-nl/windows/win32/api/shellapi/nf-shellapi-shellexecutea"/>.
         /// </summary>
-        private static readonly char[] _winChars = new char[6]
-        {
-            '\n',
-            '\r',
-            '&',
-            '^',
-            '\\',
-            ';'
-        };
+        /// <param name="hWnd">A handle to the parent window used for displaying a UI or error messages. This value can be NULL if the operation is not associated with a window.</param>
+        /// <param name="lpOperation">A pointer to a null-terminated string, referred to in this case as a verb, that specifies the action to be performed. The set of available verbs depends on the particular file or folder. Generally, the actions available from an object's shortcut menu are available verbs.</param>
+        /// <param name="lpFile">A pointer to a null-terminated string that specifies the file or object on which to execute the specified verb. To specify a Shell namespace object, pass the fully qualified parse name. Note that not all verbs are supported on all objects. For example, not all document types support the "print" verb. If a relative path is used for the lpDirectory parameter do not use a relative path for lpFile.</param>
+        /// <param name="lpParameters">If lpFile specifies an executable file, this parameter is a pointer to a null-terminated string that specifies the parameters to be passed to the application. The format of this string is determined by the verb that is to be invoked. If lpFile specifies a document file, lpParameters should be NULL.</param>
+        /// <param name="lpDirectory">A pointer to a null-terminated string that specifies the default (working) directory for the action. If this value is NULL, the current working directory is used. If a relative path is provided at lpFile, do not use a relative path for lpDirectory.</param>
+        /// <param name="nShowCmd">The flags that specify how an application is to be displayed when it is opened. If lpFile specifies a document file, the flag is simply passed to the associated application. It is up to the application to decide how to handle it. It can be any of the values that can be specified in the nCmdShow parameter for the ShowWindow function.</param>
+        /// <returns></returns>
+        [DllImport("Shell32.dll", BestFitMapping = true, CallingConvention = CallingConvention.Winapi, CharSet = CharSet.Unicode)]
+        private static extern long ShellExecute(int hWnd, string? lpOperation, string lpFile, string? lpParameters, string? lpDirectory, long nShowCmd);
 
         /// <summary>
         /// The <see cref="Process"/> used to start a browser instance with.
@@ -33,7 +34,7 @@ namespace BrowserInterface
         private readonly Process _process;
 
         /// <summary>
-        /// The <see cref="StringBuilder"/> used to construct the final command.
+        /// The <see cref="StringBuilder"/> used to construct the final url.
         /// </summary>
         private readonly StringBuilder _stringBuilder;
 
@@ -62,27 +63,31 @@ namespace BrowserInterface
         }
 
         /// <summary>
-        /// Opens the <paramref name="url"/> in the default browser of the user, with the provided <paramref name="queryParams"/>. <br/>
+        /// Opens the <paramref name="urlBase"/> in the default browser of the user, with the provided <paramref name="queryParams"/>. <br/>
         /// This method works on mac, unix and windows.
-        /// Elements in the <paramref name="queryParams"/> dictionary will be converted to their string representation, but are <see cref="object"/> to allow for more concise method calls.
+        /// Keys in the <paramref name="queryParams"/> dictionary will be converted to their string representation, but are <see cref="object"/> to allow for more concise method calls.
         /// </summary>
-        /// <param name="url">The url to open.</param>
+        /// <param name="urlBase">The url to open.</param>
         /// <param name="queryParams">The query parameters to append to the url.</param>
         /// <exception cref="PlatformNotSupportedException">If the platform is not unix, windows or mac.</exception>
-        /// <exception cref="FormatException">If the <paramref name="url"/> is not a http or https url.</exception>
-        public void OpenUrl(string url, Dictionary<string, object>? queryParams = null)
+        /// <exception cref="ArgumentException">If the <paramref name="urlBase"/> is not a http or https url.</exception>
+        /// <exception cref="FormatException"> If the <paramref name="urlBase"/> is not a valid <see cref="Uri"/>. </exception>
+        /// <exception cref="InvalidOperationException"> If the <paramref name="queryParams"/> contain duplicate keys after coalescing. </exception>
+        public void OpenUrl(string urlBase, Dictionary<string, object>? queryParams = null)
         {
+            string url = this.FormUrl(urlBase, queryParams);
+
             if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
             {
-                this.OpenUrlWindows(this.SanitizeInput(_winChars, url, "^&", queryParams));
+                OpenUrlWindows(url);
             }
             else if (RuntimeInformation.IsOSPlatform(OSPlatform.FreeBSD) || RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
             {
-                this.OpenUrlUnix(this.SanitizeInput(Array.Empty<char>(), url, "&", queryParams));
+                this.OpenUrlUnix(url);
             }
             else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
             {
-                this.OpenUrlUnix(this.SanitizeInput(Array.Empty<char>(), url, "&", queryParams));
+                this.OpenUrlUnix(url);
             }
             else
             {
@@ -91,21 +96,19 @@ namespace BrowserInterface
         }
 
         /// <summary>
-        /// Sanitizes the input url and query parameters, to circumvent possible security holes.
+        /// Forms a url out of a <paramref name="urlBase"/>, an optional collection of <paramref name="queryParams"/> and a <paramref name="paramSeparator"/>.
         /// </summary>
-        /// <param name="forbiddenCharacters">The characters that are not allowed in the url or the query parameter's string repsentation.</param>
-        /// <param name="url">The url that will be sanitized.</param>
+        /// <param name="urlBase">The base of the url..</param>
         /// <param name="queryParams">The (optional) query parameters that will be sanitized.</param>
         /// <returns>A <see cref="string"/> which contains the sanitized url and optionally appended query parameters.</returns>
-        /// <exception cref="InvalidOperationException">Thrown iff key colission occurs during key parameter sanitization.</exception>
-        /// <exception cref="FormatException">If the <paramref name="url"/> is not a http or https url.</exception>
-        private string SanitizeInput(char[] forbiddenCharacters, string url, string paramSeparator, Dictionary<string, object>? queryParams = null)
+        /// <exception cref="ArgumentException">If the <paramref name="urlBase"/> is not a http or https url.</exception>
+        /// <exception cref="FormatException"> If the <paramref name="urlBase"/> is not a valid <see cref="Uri"/>. </exception>
+        /// <exception cref="InvalidOperationException"> If the <paramref name="queryParams"/> contain duplicate keys after coalescing. </exception>
+        private string FormUrl(string urlBase, Dictionary<string, object>? queryParams = null)
         {
-            string urlOut = url.Filter(forbiddenCharacters);
-
             try
             {
-                Uri uri = new Uri(urlOut);
+                Uri uri = new Uri(urlBase);
 
                 if (uri.Scheme is not "https" and not "http")
                 {
@@ -118,35 +121,25 @@ namespace BrowserInterface
             }
 
             this._stringBuilder.Clear();
-            this._stringBuilder.Append(urlOut);
+            this._stringBuilder.Append(urlBase);
 
-            // Note: once dll importing is used on windows, sanitization can be removed entirely.
-            // Coalescing should still be done, which might still provide an InvalidOperationException.
-            // Allowing object is useful, because it makes for more concise function calls (with string builders, or streams, for instance).
-
-            Dictionary<string, string> paramOut = new Dictionary<string, string> { };
-
-            if (queryParams is Dictionary<string, object> { Count: > 0 } @params &&
-                @params.Select(kvp => paramOut.TryAdd(kvp.Key.Filter(forbiddenCharacters),
-                                                      kvp.Value.ToString().Filter(forbiddenCharacters)))
-                       .Any(k => !k))
+            if (queryParams is Dictionary<string, object> { Count: > 0 } @params)
             {
-                throw new InvalidOperationException($"Coalescing [{queryParams}] resulted in key colission!");
-            }
-
-            if (paramOut.Count > 0)
-            {
+                if (@params.Keys.GroupBy(k => k).Any(c => c.Count() > 1))
+                {
+                    throw new InvalidOperationException($"Coalescing [{queryParams}] resulted in key colission!");
+                }
                 this._stringBuilder.Append('?');
 
-                foreach (KeyValuePair<string, string> kvp in paramOut)
+                foreach (KeyValuePair<string, object> kvp in @params)
                 {
                     this._stringBuilder.Append(kvp.Key);
                     this._stringBuilder.Append('=');
                     this._stringBuilder.Append(kvp.Value);
-                    this._stringBuilder.Append(paramSeparator);
+                    this._stringBuilder.Append('&');
                 }
 
-                this._stringBuilder.Length -= paramSeparator.Length;
+                this._stringBuilder.Length -= 1;
             }
 
             return this._stringBuilder.ToString();
@@ -186,14 +179,12 @@ namespace BrowserInterface
         /// This method only functions on windows.
         /// </summary>
         /// <param name="url">The url to open.</param>
-        private void OpenUrlWindows(string url)
+        private static void OpenUrlWindows(string url)
         {
-            this._process.StartInfo.FileName = "cmd";
-            this._process.Start();
-            this._process.StandardInput.WriteLine($"start {url}");
-            this._process.StandardInput.Flush();
-            this._process.StandardInput.Close();
-            this._process.WaitForExit();
+            if (ShellExecute(0, null, url, null, null, 1) < 32)
+            {
+                Console.Error.WriteLine($"Warning: opening {url} failed with exit code {ShellExecute(0, null, url, null, null, 1)}!");
+            }
         }
 
         /// <summary>
